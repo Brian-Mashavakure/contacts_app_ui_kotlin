fb9673cdfecb36097bb458d2e82ec06370a0ba29
Brian-Mashavakure <brianmashavakure@gmail.com>
2023-11-04 22:14:53 +0200

    First Commit

---
+++ contacts_app_ui/app/src/main/java/com/contacts_app_ui/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.contacts_app_ui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.contacts_app_ui.ui.theme.Contacts_app_uiTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+    }
+}
+
